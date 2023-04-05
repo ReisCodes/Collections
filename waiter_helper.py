e41249f62e43590a23b543b056b94c012420b008
Reis Pinnock <reis_pinnock@hotmail.co.uk>
2023-04-05 10:27:08 +0100

    ready to upload to git

--- waiter_helper.py
+++ waiter_helper.py
@@ -1,60 +1,61 @@
 starters = ["garlic bread", "calamari", "cheese balls", "prawn cocktail", "soup"]
 mains = ["spaghetti alfredo", "hunters chicken", "sausage and mash", "burger and chips", "steak"]
 desserts = ["chocolate cake", "eton mess", "cheesecake", "victoria sponge", "sticky toffee pudding"]
+drinks = ["water", "coke", "lemonade", "beer", "wine"]
 
 order = []
 
-print('''
-Starters:         
-garlic bread      
-calamari
-cheese balls
-prawn cocktail
-soup
-''')
+print("Starters:")
+for starter in starters:
+    print(starter)
 
-starter_choice = input('''What starter would you like to order? 
+while True:
+    starter_choice = input('''What starter would you like to order? 
                        : ''')
-order.append(starter_choice)
+    if starter_choice not in starters:
+        print("This isn't a valid option, please try again")
+    else:
+        order.append(starter_choice)
+        break
 
-print('''
-Mains:         
-spaghetti alfredo      
-hunters chicken
-sausage and mash
-burger and chips
-steak
-''')
+print("Mains:")
+for main in mains:
+    print(main)
 
-main_choice = input('''What main would you like to order? 
+while True:
+    main_choice = input('''What main would you like to order? 
                        : ''')
-order.append(main_choice)
+    if main_choice not in mains:
+        print("This isn't a valid option, please try again")
+    else:
+        order.append(main_choice)
+        break
 
-print('''
-Desserts:         
-chocolate cake      
-eton mess
-cheesecake
-victoria sponge
-sticky toffee pudding
-''')
+print("Desserts:")
+for dessert in desserts:
+    print(dessert)
 
-dessert_choice = input('''What dessert would you like to order? 
+while True:
+    dessert_choice = input('''What dessert would you like to order? 
                        : ''')
-order.append(dessert_choice)
+    if dessert_choice not in desserts:
+        print("This isn't a valid option, please try again")
+    else:
+        order.append(dessert_choice)
+        break
 
-print('''
-Drinks:         
-water      
-coke
-lemonade
-beer
-wine
-''')
+print("Drinks:")
+for drink in drinks:
+    print(drink)
 
-drink_choice = input('''What drink would you like to order? 
+while True:
+    drink_choice = input('''What drink would you like to order? 
                        : ''')
-order.append(drink_choice)
+    if drink_choice not in drinks:
+        print("This isn't a valid option, please try again")
+    else:
+        order.append(drink_choice)
+        break
 
 print(f'''Here is your order:
 Starter: {order[0]}
